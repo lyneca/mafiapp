bd16dec33594897c513b53746799f9b53d2d3a76
Luke Tuthill <lukegithub@gmail.com>
2016-01-22 11:30:17 +1100

    added vote model and migrated models

--- voting/models.py
+++ voting/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Vote(models.Model):
+    voter = models.ForeignKey(User, related_name='voter')
+    votee = models.ForeignKey(User, related_name='votee')
+    time = models.DateTimeField(auto_now_add=True)
