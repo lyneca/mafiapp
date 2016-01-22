1fb4eb8d5652773bf0bacd9b48767e6e6bc5d674
Luke Tuthill <lukegithub@gmail.com>
2016-01-22 16:24:07 +1100

    logging in works now

--- voting/urls.py
+++ voting/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^/vote/', views.vote, name='vote')
+    url(r'^vote/', views.vote, name='vote'),
+    url(r'^login/', views.log_in_user, name='login')
 ]
