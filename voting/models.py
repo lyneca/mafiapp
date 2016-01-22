c92a99579652741f46bd30c829ae24ca5906feaf
Luke Tuthill <lukegithub@gmail.com>
2016-01-22 11:45:58 +1100

    implemented vote model in template, created test vote and users

--- voting/models.py
+++ voting/models.py
@@ -7,3 +7,6 @@ class Vote(models.Model):
     voter = models.ForeignKey(User, related_name='voter')
     votee = models.ForeignKey(User, related_name='votee')
     time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.voter.get_full_name() + ' voted for ' + self.votee.get_full_name()
--- voting/views.py
+++ voting/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Vote
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    votes = Vote.objects.order_by('time')
+    context = {
+        'votes': votes
+    }
+    return render(request, 'index.html', context=context)
