2841c13a9580976e206117909d7a01c2a9fd3ce7
Luke Tuthill <lukegithub@gmail.com>
2016-02-22 12:42:59 +1100

    updated passwords for next game

--- read_from_csv.py
+++ read_from_csv.py
@@ -1,23 +1,22 @@
+import random
+
 from django.contrib.auth.models import User
 
 with open('appform.csv') as file:
     for line in file:
-        time, fname, lname, email, slack, apt, orig = line.strip().split(',')
+        time, fname, lname, email, act = line.strip().split(',')
         if not time == 'Timestamp':
-            print('adding user', fname.lower())
-            u = User(first_name=fname.capitalize(),
-                     last_name=lname.capitalize(),
-                     username=fname.lower(), email=email
+            print('adding user', fname.strip('"').lower() + '_' + lname.strip('"').lower()[0])
+            u = User(first_name=fname.strip('"').capitalize(),
+                     last_name=lname.strip('"').capitalize(),
+                     username=fname.strip('"').lower() + '_' + lname.strip('"').lower()[0], email=email.strip('"')
                      )
             u.set_password('abcde')
             u.save()
 
-import random
-
 i = 0
-random.seed('a')
 for user in User.objects.filter(is_superuser=False):
-    pwd = hex(random.randint(16 ** 8, 16 ** 9 - 1))
+    pwd = hex(random.randint(16 ** 5, 16 ** 6 - 1))[2:]
     user.set_password(pwd)
     user.save()
     print(user.first_name, pwd)
